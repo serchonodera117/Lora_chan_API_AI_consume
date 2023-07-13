a1c5e782d930b8e70460e9df8156837eb8e002a9
serchonodera117 <sergio.lobo117@gmail.com>
2023-07-12 20:09:23 -0600

    fixed bug of listening channels, now can receive a voice command and write sentences at the same time

--- Assets/Scripts/Animation_Script.cs
+++ Assets/Scripts/Animation_Script.cs
@@ -10,11 +10,30 @@ using JetBrains.Annotations;
 using LMNT;
 using System.Runtime.CompilerServices;
 using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Linq;
-using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEditor.VersionControl;
+using System.IO;
+using Unity.VisualScripting;
+
+
+
+
 
 public class Animation_Script : MonoBehaviour
 {
+    private const string APIKey = "";
+    private const string ChatGPTEndpoint = "https://api.openai.com/v1/chat/completions";
+    private const string organization = "";
+
+
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
     private Animator anim;
     private bool isListening = false;
     private bool isTalking = false;
@@ -35,12 +54,13 @@ public class Animation_Script : MonoBehaviour
     public Sprite listenigDisabled;
     public Button btnListening;
     public TextMeshProUGUI listeningMessage;
-    public TMP_InputField  writingTextBox; 
+    public TMP_InputField writingTextBox;
 
     // Start is called before the first frame update
     void Start()
     {
-        fristCommand= new Dictionary<string, Action>();
+        writingTextBox.text = "";
+        fristCommand = new Dictionary<string, Action>();
         fristCommand.Add("lora chan", prepareTotakeInstructions);
         fristCommand.Add("lora", prepareTotakeInstructions);
 
@@ -52,6 +72,8 @@ public class Animation_Script : MonoBehaviour
         dictationRecognizer = new DictationRecognizer();
         dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
 
+        PhraseRecognitionSystem.Restart();
+
         anim = GetComponent<Animator>();
         StartCoroutine(no_loop_bodyAimation("greeting", "Standing_Greeting")); //call the function with timmer to count aniamtion seconds
         anim.SetTrigger("face_greeting");
@@ -62,11 +84,13 @@ public class Animation_Script : MonoBehaviour
     {
         if (isTalking)
         {
-          speechTimmer += Time.deltaTime;
-           if(speechTimmer >= secondsTak) { stopTalking(speechTimmer, secondsTak); speechTimmer = 0; }
+            speechTimmer += Time.deltaTime;
+            if (speechTimmer >= secondsTak) { stopTalking(speechTimmer, secondsTak); speechTimmer = 0; }
         }
     }
 
+
+    //-----------------------------Animations
 
     public IEnumerator no_loop_bodyAimation(string nameExpression, string nameAnimation)
     {
@@ -97,6 +121,7 @@ public class Animation_Script : MonoBehaviour
         anim.SetTrigger(nameExpression);
     }
 
+    //-------------------------Turn on and off Command recognize
     public void activate_disactivate_listening()
     {
         isListening = !isListening;
@@ -108,8 +133,9 @@ public class Animation_Script : MonoBehaviour
             writingTextBox.readOnly = true;
             loop_FaceExpressions("face_listening");
             isComandActive = true;
-            //voicerecognicer.Start();
-            dictationRecognizer.Start();
+            voicerecognicer.Start();
+            //dictationRecognizer.Start();
+            StartCoroutine(openNotepad("Mensaje escrito UW U"));
         }
         else
         {
@@ -119,22 +145,23 @@ public class Animation_Script : MonoBehaviour
             loop_bodyAnimation("idle");
             loop_FaceExpressions("face_base");
             isComandActive = false;
-            //voicerecognicer.Stop();
-            dictationRecognizer.Stop();
+            voicerecognicer.Stop();
+            //dictationRecognizer.Stop();
         }
     }
 
-   public  void Talk(string theSpeech)
+    //-------------------------------------------------------Talking
+    public void Talk(string theSpeech)
     {
         loop_FaceExpressions("face_talking");
         isTalking = true;
         float charactersPerSecond = 15f;
         float duration = theSpeech.Length / charactersPerSecond;
-       secondsTak =  duration;
+        secondsTak = duration;
         voice.Speak(theSpeech, SpeechVoiceSpeakFlags.SVSFlagsAsync);
     }
 
-    public void stopTalking(float timmer, float speechDuration)//Stop Talking
+    public void stopTalking(float timmer, float speechDuration)
     {
         isTalking = false;
         timmer = 0f;
@@ -144,35 +171,160 @@ public class Animation_Script : MonoBehaviour
     }
     public void onRecognizeLORAcommand(PhraseRecognizedEventArgs word)//executes prepareTotakeInstructions
     {
-        Debug.Log(word);
-        fristCommand[word.text].Invoke();
         if (isComandActive) { voicerecognicer.Stop(); isComandActive = false; }
+        if (isDictationActive) { dictationRecognizer.Stop(); isComandActive = false; }
+
+        fristCommand[word.text].Invoke();
     }
     public void prepareTotakeInstructions()//listening commands 
     {
         loop_bodyAnimation("idle_listening");
+        PhraseRecognitionSystem.Shutdown();
 
-       // PhraseRecognitionSystem.Shutdown();
-
-        //if (voicerecognicer.IsRunning) { voicerecognicer.Stop(); isComandActive = false; }
-        //if (!isDictationActive) { dictationRecognizer.Start(); isDictationActive = true; }
+        if (voicerecognicer.IsRunning) {
+            voicerecognicer.Stop();
+            isComandActive = false;
+        }
+        if (!isDictationActive) {
+            dictationRecognizer = new DictationRecognizer();
+            dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+            dictationRecognizer.Start();
+            isDictationActive = true;
+        }
     }
 
-
+    public void loraThabks()
+    {
+        //StartCoroutine(no_loop_bodyAimation("thinking", "Thinking"));
+    }
 
     //-----------------------------------------------------dictation recognizer
     private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
     {
 
         writingTextBox.text = text;
-        //dictationRecognizer.Stop();
-        //isDictationActive = false;
+        dictationRecognizer.Stop();
+        dictationRecognizer.Dispose();
+        isDictationActive = false;
 
-        //if (!voicerecognicer.IsRunning)
-        //{
-        //    voicerecognicer.Start();
-        //    isComandActive = true;
-        //}
+        loop_bodyAnimation("thinking");
+
+
+        //call an httpRequest
+        if (!voicerecognicer.IsRunning)
+        {
+            PhraseRecognitionSystem.Restart();
+            voicerecognicer.Start();
+            isComandActive = true;
+        }
     }
+    //request of buttom
+    public void requestByButton()
+    {
+        string textBoxContent = writingTextBox.text.Trim();
+        if (!string.IsNullOrEmpty(textBoxContent)) {
+            StartCoroutine(readLoraResponse(writingTextBox.text));
+        }
+        writingTextBox.text = "";
+    }
+
+    //----------------------------------------http request to  Open AI API
+    private async Task<string> SendChatGptRequest(string message)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {APIKey}");
+           client.DefaultRequestHeaders.Add("OpenAI-Organization", organization);
+            var requestData = new ChatGptRequestData
+            {
+                messages = new ChatGptMessage[]
+                {
+                    new ChatGptMessage
+                    {
+                        role="system",
+                        content="You are a helpful assistant"
+                    },
+                    new ChatGptMessage
+                    {
+                      role="user",
+                      content = message
+                    }
+                },
+                model = "gpt-3.5-turbo"
+            };
+            var json = JsonUtility.ToJson(requestData);
+            UnityEngine.Debug.Log(message);
+            UnityEngine.Debug.Log("requested data" + requestData);
+            UnityEngine.Debug.Log("json to string" + json.ToString());
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(ChatGPTEndpoint, data);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return responseContent;
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"Chat GPT API request failed with status code: {response.StatusCode}");
+
+                if (response.StatusCode.ToString() == "TooManyRequests") {
+                    Talk("Lo lamento, ahora mismo ya no me encuentro disponible para responder peticiones, intenta más tarde");
+                }
+                else
+                {
+                    Talk("Me disculpo, algo ha salido mal, y no he podido procesar tu petición");
+
+                }
+                return null;
+            }
+        }
+    }
+
+  
+
+    private IEnumerator readLoraResponse(string message)
+    {
+        string input = message;
+        Task<string> responseTask = SendChatGptRequest(input); 
+        
+        yield return new WaitUntil(()=> responseTask.IsCompleted); 
+        
+        string response = responseTask.Result;
+        if (response!=null)
+        {
+            StartCoroutine(openNotepad(response.ToSafeString()));
+            UnityEngine.Debug.Log(response);
+        }
+    }
+    //--------------------------------Open and Write Noteblock
+    private IEnumerator openNotepad(string writedMessage){
+        string tempFilepath = Path.GetTempFileName();
+        File.WriteAllText(tempFilepath, writedMessage);
+
+        Process process = Process.Start("notepad.exe", tempFilepath);
+        
+        yield return new WaitForSeconds(1);//wait a little time to ensure that is already open
+
+        File.Delete(tempFilepath);
+    }
+
 }
 
+
+[System.Serializable]
+public class ChatGptRequestData
+{
+    public ChatGptMessage[] messages;
+    public string model;
+}
+
+[System.Serializable]
+public class ChatGptMessage
+{
+    public string role;
+    public string content;
+}
